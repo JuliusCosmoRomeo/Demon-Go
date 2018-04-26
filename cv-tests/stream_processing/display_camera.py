30fdb2e4d7b1d7cf39f1a9d7879ad9c6c4bb99c9
philipp.bode <philipp.bode@student.hpi.de>
2018-04-26 23:16:14 +0200

    Add rectangle detection, dewarp and noise estimator for document recognition.

---
+++ cv-tests/stream_processing/display_camera.py
@@ -0,0 +1,63 @@
+
+"""
+Display the content of the webcam stream while running text detection
+in an additional process that returns coordinates of recognized text fields.
+"""
+
+import cv2
+import numpy as np
+import imutils
+
+from time import sleep
+from urllib.request import urlopen
+from urllib.error import URLError
+
+from draw_contours import draw_contours
+from rectangles import (
+    RectangleDrawer
+)
+
+url = 'http://192.168.1.9:8080/'
+
+def fetch_stream():
+    r = RectangleDrawer()
+    cv2.namedWindow('Video')
+    cv2.createTrackbar('AREA', 'Video', 7000, 100000, lambda x: None)
+    cv2.createTrackbar('thrs1', 'Video', 70, 100, lambda x: None)
+
+
+    while True:
+        try:
+            stream = urlopen(url)
+            break
+        except URLError:
+            print('Stream could not be opened, retrying in 3')
+            sleep(3)
+    data = b''
+    while True:
+        data += stream.read(10240)
+        a = data.find(b'\xff\xd8')
+        b = data.find(b'\xff\xd9')
+        if a != -1 and b != -1:
+            jpg = data[a:b+2]
+            data = data[b+2:]
+            img = imutils.rotate_bound(
+                cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR),
+                90
+            )
+
+            draw_contours(img)
+            cv2.setMouseCallback('Video', r.on_mouse, 0)
+            r.draw(img)
+            cv2.imshow('Video', img)
+            key = cv2.waitKey(1) 
+            if key == 27:
+                exit(0)
+            if key == 114:
+                r.reset()
+
+def main():
+    fetch_stream()
+
+if __name__ == '__main__':
+    main()
---
+++ cv-tests/stream_processing/estimators.py
@@ -0,0 +1,19 @@
+import math
+
+import cv2
+import numpy as np
+from scipy.signal import convolve2d
+
+def estimate_noise(img):
+	# as per https://stackoverflow.com/a/25436112
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    H, W = img.shape[:2]
+
+    M = np.array([[1, -2, 1],
+                  [-2, 4, -2],
+                  [1, -2, 1]])
+
+    sigma = np.sum(np.sum(np.absolute(convolve2d(img, M))))
+    sigma = sigma * math.sqrt(0.5 * math.pi) / (6 * (W-2) * (H-2))
+
+    return sigma
