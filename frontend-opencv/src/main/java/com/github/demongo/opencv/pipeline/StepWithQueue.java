7b296412c1b9efcf81e1136b33ac63ce552c9090
hendraet <hendrik.raetz@niederjahna.de>
2018-05-30 13:30:33 +0200

    Adds queue step and adds untested server improvements

--- frontend-opencv/src/main/java/com/github/demongo/opencv/pipeline/StepWithQueue.java
+++ frontend-opencv/src/main/java/com/github/demongo/opencv/pipeline/StepWithQueue.java
@@ -1,9 +1,33 @@
 package com.github.demongo.opencv.pipeline;
 
-public abstract class StepWithQueue extends Step {
-//    void queue(Snapshot snapshot) {
-//    }
-//
-//    Snapshot getBest() {
-//    }
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class SnapshotComparator implements Comparator<Snapshot> {
+    @Override
+    public int compare(Snapshot a, Snapshot b) {
+        return (int) Math.signum(a.score - b.score);
+    }
+}
+
+abstract class StepWithQueue extends Step {
+    PriorityQueue<Snapshot> snapshotQueue;
+
+    StepWithQueue() {
+       this.snapshotQueue = new PriorityQueue<>(10, new SnapshotComparator());
+    }
+
+    void queue(Snapshot snapshot) {
+        snapshotQueue.add(snapshot);
+    }
+
+    Snapshot getBest() {
+        return this.snapshotQueue.poll();
+    }
+
+    Snapshot getBestAndClear() {
+        Snapshot best = this.snapshotQueue.peek();
+        this.snapshotQueue.clear();
+        return best;
+    }
 }
--- server_flask/server_flask.py
+++ server_flask/server_flask.py
@@ -1,19 +1,33 @@
 import base64
 from datetime import datetime
 
+import os
 from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/post', methods=['GET', 'POST'])
-def test():
-    image_string = request.form['image']
+def save_image(image_string):
+    filename = "images/" + datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    ending = ".txt"
+    suffix = ""
 
-    # TODO: currently overwrites pictures sent at the same time
-    ts = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-    with open("images/" + ts + ".jpg", "wb") as f:
+    if os.path.isfile(filename + ending):
+        index = 1
+        suffix = "(" + str(index) + ")"
+        while os.path.isfile(filename + suffix + ending):
+            index += 1
+            suffix = "(" + str(index) + ")"
+
+    with open(filename + suffix + ending, "wb") as f:
         f.write(base64.b64decode(image_string))
+
+
+@app.route('/post', methods=['GET', 'POST'])
+def get_image():
+    image_string = request.form['image']
+    save_image(image_string)
+
     return 'gotcha'
 
 
