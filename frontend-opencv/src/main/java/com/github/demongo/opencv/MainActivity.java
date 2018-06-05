0ae2c7f0bc6203ec91d804fa3885fae1602eb9fc
Tom Beckmann <tomjonabc@gmail.com>
2018-06-05 11:11:05 +0200

    add pipeline class in pipeline package

--- frontend-opencv/src/main/java/com/github/demongo/opencv/MainActivity.java
+++ frontend-opencv/src/main/java/com/github/demongo/opencv/MainActivity.java
@@ -3,7 +3,6 @@ package com.github.demongo.opencv;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -11,18 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.WindowManager;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.Volley;
-import hpi.gitlab.demongo.pipeline.BlurEstimationStep;
-import hpi.gitlab.demongo.pipeline.BrandDetectionStep;
-import hpi.gitlab.demongo.pipeline.ContourDetectionStep;
-import hpi.gitlab.demongo.pipeline.NoiseEstimationStep;
-import hpi.gitlab.demongo.pipeline.SendingStep;
-import hpi.gitlab.demongo.pipeline.Snapshot;
-import hpi.gitlab.demongo.pipeline.Step;
+import hpi.gitlab.demongo.pipeline.Pipeline;
 
-import org.apache.commons.collections4.queue.CircularFifoQueue;
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
@@ -31,21 +20,12 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
 
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity implements CvCameraViewListener2 {
 
     private CameraBridgeViewBase openCvCameraView;
     private Mat currentFrame;
-    private BrandDetectionStep brandDetectionStep;
-    private NoiseEstimationStep noiseEstimationStep;
-    private BlurEstimationStep blurEstimationStep;
-    private ContourDetectionStep contourDetectionStep;
-    private Step firstStep;
-    SendingStep sendingStep;
-    private RequestQueue requestQueue;
-    private CircularFifoQueue<Mat> nextFrames;
     private static final String TAG = MainActivity.class.getName();
+    private Pipeline pipeline;
 
     //this callback is needed because Android's onCreate is called before OpenCV is loaded
     //-> hence Mat-initialization with "new Mat()" fails in onCreate
@@ -57,34 +37,8 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
                 {
                     Log.i("OpenCV", "OpenCV loaded successfully");
 
-                    brandDetectionStep = new BrandDetectionStep(getApplicationContext(), new ArrayList<String>(){{
-                        add("template");
-                        add("mate_label");
-                        add("mate_flasche");
-                    }});
-
-                    noiseEstimationStep = new NoiseEstimationStep();
-                    blurEstimationStep = new BlurEstimationStep();
-                    contourDetectionStep = new ContourDetectionStep();
-                    sendingStep = new SendingStep(requestQueue);
-
-                    blurEstimationStep
-                            .next(contourDetectionStep)
-                            .next(noiseEstimationStep)
-                            .next(sendingStep);
-                    blurEstimationStep
-                            .next(brandDetectionStep)
-                            .next(sendingStep);
-                    nextFrames = new CircularFifoQueue<>(10);
+                    pipeline = new Pipeline(getApplicationContext());
                     openCvCameraView.enableView();
-
-                    firstStep = blurEstimationStep;
-                    firstStep.setMeasureTime(true);
-                    firstStep.setNextFrames(nextFrames);
-                    Thread pipelineThread = new Thread(firstStep);
-                    pipelineThread.start();
-
-
                 } break;
                 default:
                 {
@@ -107,8 +61,6 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
         openCvCameraView = (CameraBridgeViewBase) findViewById(R.id.activity_surface_view);
         openCvCameraView.setVisibility(CameraBridgeViewBase.VISIBLE);
         openCvCameraView.setCvCameraViewListener(this);
-
-        this.requestQueue = Volley.newRequestQueue(this);
     }
 
     @Override
@@ -116,7 +68,7 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
         super.onDestroy();
         openCvCameraView.disableView();
 
-        this.sendingStep.cancelExecutorService();
+        this.pipeline.destroy();
     }
 
     @Override
@@ -144,7 +96,7 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
     public Mat onCameraFrame(CvCameraViewFrame frame) {
         currentFrame = frame.rgba();
 
-        nextFrames.add(currentFrame);
+        this.pipeline.add(currentFrame);
         //blurEstimationStep.process(new Snapshot(currentFrame,1));
 
         return currentFrame;
---
+++ pipeline/src/main/java/hpi/gitlab/demongo/pipeline/Pipeline.java
@@ -0,0 +1,62 @@
+package hpi.gitlab.demongo.pipeline;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import org.apache.commons.collections4.queue.CircularFifoQueue;
+import org.opencv.core.Mat;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class Pipeline {
+
+    private BrandDetectionStep brandDetectionStep;
+    private NoiseEstimationStep noiseEstimationStep;
+    private BlurEstimationStep blurEstimationStep;
+    private ContourDetectionStep contourDetectionStep;
+    private Step firstStep;
+    SendingStep sendingStep;
+    private RequestQueue requestQueue;
+    private CircularFifoQueue<Mat> nextFrames;
+
+    public Pipeline(Context context) {
+        this.requestQueue = Volley.newRequestQueue(context);
+
+        brandDetectionStep = new BrandDetectionStep(context, new ArrayList<String>(){{
+            add("template");
+            add("mate_label");
+            add("mate_flasche");
+        }});
+
+        noiseEstimationStep = new NoiseEstimationStep();
+        blurEstimationStep = new BlurEstimationStep();
+        contourDetectionStep = new ContourDetectionStep();
+        sendingStep = new SendingStep(requestQueue);
+
+        blurEstimationStep
+                .next(contourDetectionStep)
+                .next(noiseEstimationStep)
+                .next(sendingStep);
+        blurEstimationStep
+                .next(brandDetectionStep)
+                .next(sendingStep);
+        nextFrames = new CircularFifoQueue<>(10);
+
+        firstStep = blurEstimationStep;
+        firstStep.setMeasureTime(true);
+        firstStep.setNextFrames(nextFrames);
+        Thread pipelineThread = new Thread(firstStep);
+        pipelineThread.start();
+
+    }
+
+    public void destroy() {
+        this.sendingStep.cancelExecutorService();
+    }
+
+    public void add(Mat frame) {
+        nextFrames.add(frame);
+    }
+}
