499b6c4826321fa93462ab280d01ce0f23ab40ec
Tom Beckmann <tomjonabc@gmail.com>
2018-06-08 12:46:01 +0200

    fix image decoding in arcore

--- android/src/com/github/demongo/ARSnapshot.java
+++ android/src/com/github/demongo/ARSnapshot.java
@@ -1,5 +1,6 @@
 package com.github.demongo;
 
+import android.graphics.ImageFormat;
 import android.media.Image;
 import android.opengl.Matrix;
 import android.util.Log;
@@ -15,6 +16,7 @@ import com.google.ar.core.exceptions.NotYetAvailableException;
 
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
@@ -22,6 +24,66 @@ import java.nio.FloatBuffer;
 import hpi.gitlab.demongo.pipeline.Snapshot;
 
 public class ARSnapshot extends Snapshot {
+    // code from http://answers.opencv.org/question/61628/android-camera2-yuv-to-rgb-conversion-turns-out-green/
+    private static Mat convertYuv420888ToMat(Image image, boolean isGreyOnly) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        Image.Plane yPlane = image.getPlanes()[0];
+        int ySize = yPlane.getBuffer().remaining();
+
+        if (isGreyOnly) {
+            byte[] data = new byte[ySize];
+            yPlane.getBuffer().get(data, 0, ySize);
+
+            Mat greyMat = new Mat(height, width, CvType.CV_8UC1);
+            greyMat.put(0, 0, data);
+
+            return greyMat;
+        }
+
+        Image.Plane uPlane = image.getPlanes()[1];
+        Image.Plane vPlane = image.getPlanes()[2];
+
+        // be aware that this size does not include the padding at the end, if there is any
+        // (e.g. if pixel stride is 2 the size is ySize / 2 - 1)
+        int uSize = uPlane.getBuffer().remaining();
+        int vSize = vPlane.getBuffer().remaining();
+
+        byte[] data = new byte[ySize + (ySize/2)];
+
+        yPlane.getBuffer().get(data, 0, ySize);
+
+        ByteBuffer ub = uPlane.getBuffer();
+        ByteBuffer vb = vPlane.getBuffer();
+
+        int uvPixelStride = uPlane.getPixelStride(); //stride guaranteed to be the same for u and v planes
+        if (uvPixelStride == 1) {
+            uPlane.getBuffer().get(data, ySize, uSize);
+            vPlane.getBuffer().get(data, ySize + uSize, vSize);
+
+            Mat yuvMat = new Mat(height + (height / 2), width, CvType.CV_8UC1);
+            yuvMat.put(0, 0, data);
+            Mat rgbMat = new Mat(height, width, CvType.CV_8UC3);
+            Imgproc.cvtColor(yuvMat, rgbMat, Imgproc.COLOR_YUV2RGB_I420, 3);
+            yuvMat.release();
+            return rgbMat;
+        }
+
+        // if pixel stride is 2 there is padding between each pixel
+        // converting it to NV21 by filling the gaps of the v plane with the u values
+        vb.get(data, ySize, vSize);
+        for (int i = 0; i < uSize; i += 2) {
+            data[ySize + i + 1] = ub.get(i);
+        }
+
+        Mat yuvMat = new Mat(height + (height / 2), width, CvType.CV_8UC1);
+        yuvMat.put(0, 0, data);
+        Mat rgbMat = new Mat(height, width, CvType.CV_8UC3);
+        Imgproc.cvtColor(yuvMat, rgbMat, Imgproc.COLOR_YUV2RGB_NV21, 3);
+        yuvMat.release();
+        return rgbMat;
+    }
 
     private float[] viewProjectionMatrix = new float[16];
     private float[] points;
@@ -52,13 +114,7 @@ public class ARSnapshot extends Snapshot {
 
     private static Mat matFromFrame(Frame frame) throws NotYetAvailableException {
         Image image = frame.acquireCameraImage();
-        Image.Plane plane = image.getPlanes()[0];
-        ByteBuffer buffer = plane.getBuffer();
-        Mat mat = new Mat(image.getHeight(), image.getWidth(), CvType.CV_8UC4);
-        byte[] pixels = new byte[buffer.limit()];
-        buffer.position(0);
-        buffer.get(pixels);
-        mat.put(0, 0, pixels);
+        Mat mat = convertYuv420888ToMat(image, false);
         image.close();
         return mat;
     }
