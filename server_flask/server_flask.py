50fbb2f05aacb9e1f687289871e26515ea62a66c
philipp.bode <philipp.bode@student.hpi.de>
2018-06-15 16:14:18 +0200

    Refactor text detection to dedicated class.

--- server_flask/server_flask.py
+++ server_flask/server_flask.py
@@ -1,85 +1,17 @@
 import base64
 import multiprocessing
-import queue
-import uuid
 
 import cv2
 import numpy as np
 from flask import Flask, request, render_template
 from flask_socketio import SocketIO
 
-import sys
-sys.path.append("./EAST")
-
-from EAST.run_demo_server import get_predictor
-
-CHECKPOINT_PATH = 'EAST/east_icdar2015_resnet_v1_50_rbox'
-
+from text_detection import TextDetection
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-
-def save_image(img):
-    filename = f'static/{uuid.uuid4()}.jpg'
-
-    cv2.imwrite(filename, img)
-    return filename
-
-
-frames = multiprocessing.Queue()
-results = multiprocessing.Queue()
-
-
-def draw_text_boxes(img, rst):
-    for t in rst['text_lines']:
-        d = np.array([t['x0'], t['y0'], t['x1'], t['y1'], t['x2'],
-                      t['y2'], t['x3'], t['y3']], dtype='int32')
-        d = d.reshape(-1, 2)
-        cv2.polylines(
-            img, [d], isClosed=True,
-            color=(255, 255, 0), thickness=3)
-    return img
-
-
-def crop_text(img, points):
-
-    files = []
-    for textbox in points:
-        min_x = 10000
-        max_x = 0
-        min_y = 10000
-        max_y = 0
-        for key, value in textbox.items():
-
-            if key.startswith('x'):
-                min_x = min(min_x, int(value))
-                max_x = max(max_x, int(value))
-            elif key.startswith('y'):
-                min_y = min(min_y, int(value))
-                max_y = max(max_y, int(value))
-            else:
-                files.append(save_image(img[min_y:max_y, min_x:max_x]))
-    results.put(files)
-
-
-def find_text():
-    prediction_function = get_predictor(CHECKPOINT_PATH)
-
-    while True:
-        try:
-            img = frames.get(timeout=0.05)
-        except queue.Empty:
-            continue
-        try:
-            rst = prediction_function(img)
-            if rst['text_lines']:
-                draw_text_boxes(img, rst)
-                filename = save_image(img)
-                # crop_text(img, rst['text_lines'])
-                results.put([filename])
-        except (cv2.error, AssertionError):
-            pass
+td = TextDetection(split=False)
 
 
 @app.route('/', methods=['GET'])
@@ -97,15 +29,15 @@ def get_image():
         cv2.IMREAD_COLOR
     )
 
-    path = save_image(img)
+    path = TextDetection.save_image(img)
     socketio.emit(
         'new image',
         {'path': path}
     )
 
-    frames.put_nowait(img.copy())
+    td.in_queue.put_nowait(img.copy())
 
-    for filename in results.get():
+    for filename in td.out_queue.get():
         socketio.emit(
             'new image',
             {'path': filename}
@@ -118,20 +50,20 @@ def get_image():
 def test():
     # print("POST-Request", request.form['score'])
 
-    img = cv2.imread('static/IMG_2000.jpg', cv2.IMREAD_COLOR)
-    frames.put_nowait(img.copy())
+    img = cv2.imread('static/TEST_IMAGE2.jpg', cv2.IMREAD_COLOR)
+    td.in_queue.put_nowait(img.copy())
 
-    filename = results.get()
-    socketio.emit(
-        'new image',
-        {'path': filename}
-    )
+    for filename in td.out_queue.get():
+        socketio.emit(
+            'new image',
+            {'path': filename}
+        )
 
     return 'gotcha'
 
 
 if __name__ == '__main__':
-    p = multiprocessing.Process(target=find_text)
+    p = multiprocessing.Process(target=td.detect_text)
     p.start()
 
     socketio.run(
---
+++ server_flask/text_detection.py
@@ -0,0 +1,94 @@
+import sys
+import queue
+import uuid
+from multiprocessing import Queue
+
+import cv2
+import numpy as np
+
+sys.path.append("./EAST")
+from EAST.run_demo_server import get_predictor
+
+import pdb
+
+class ForkedPdb(pdb.Pdb):
+    """A Pdb subclass that may be used
+    from a forked multiprocessing child
+
+    """
+    def interaction(self, *args, **kwargs):
+        _stdin = sys.stdin
+        try:
+            sys.stdin = open('/dev/stdin')
+            pdb.Pdb.interaction(self, *args, **kwargs)
+        finally:
+            sys.stdin = _stdin
+
+class TextDetection:
+
+    CHECKPOINT_PATH = 'EAST/east_icdar2015_resnet_v1_50_rbox'
+    QUEUE_TIMEOUT = 0.05
+
+    def __init__(self, in_queue=None, out_queue=None, split=False):
+        self.in_queue = in_queue or Queue()
+        self.out_queue = out_queue or Queue()
+        self.process = self.crop_text if split else self.draw_text_boxes
+
+    @staticmethod
+    def save_image(img):
+        filename = f'static/{uuid.uuid4()}.jpg'
+
+        cv2.imwrite(filename, img)
+        return filename
+
+    def draw_text_boxes(self, img, rst):
+        for t in rst:
+            d = np.array([t['x0'], t['y0'], t['x1'], t['y1'], t['x2'],
+                          t['y2'], t['x3'], t['y3']], dtype='int32')
+            d = d.reshape(-1, 2)
+            cv2.polylines(
+                img, [d], isClosed=True,
+                color=(255, 255, 0), thickness=3)
+        filename = self.save_image(img)
+
+        self.out_queue.put([filename])
+
+    def crop_text(self, img, points):
+
+        def clamp(n, largest):
+            return max(0, min(int(n), largest))
+
+        HEIGHT, WIDTH, _ = img.shape
+
+        files = []
+        for textbox in points:
+            min_x = 10000
+            max_x = 0
+            min_y = 10000
+            max_y = 0
+            for k, v in textbox.items():
+
+                if k.startswith('x'):
+                    min_x = min(min_x, clamp(v, WIDTH))
+                    max_x = max(max_x, clamp(v, WIDTH))
+                elif k.startswith('y'):
+                    min_y = min(min_y, clamp(v, HEIGHT))
+                    max_y = max(max_y, clamp(v, HEIGHT))
+                else:
+                    files.append(self.save_image(
+                        img[min_y:max_y, min_x:max_x]
+                    ))
+
+        self.out_queue.put(files)
+
+    def detect_text(self):
+        prediction_function = get_predictor(self.CHECKPOINT_PATH)
+
+        while True:
+            try:
+                img = self.in_queue.get(timeout=self.QUEUE_TIMEOUT)
+            except queue.Empty:
+                continue
+            rst = prediction_function(img)['text_lines']
+            if rst:
+                self.process(img, rst)
